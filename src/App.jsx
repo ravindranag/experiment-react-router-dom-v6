be4cafbcf8a83f33a05085a82224092fe81c8a7a
Ravindra Nag <ravindranag52@gmail.com>
2022-06-18 12:12:00 +0530

    BrowserRouter as Router

--- src/App.jsx
+++ src/App.jsx
@@ -1,23 +1,32 @@
 import { Container } from "@mui/material";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { BrowserRouter as Router, Routes, Route, useMatch } from "react-router-dom";
 import RouteController from "./controllers/RouteController";
 import Article from "./pages/Article";
 import ArticleList from "./pages/ArticleList";
 import Home from "./pages/Home";
 import NotFound from "./pages/NotFound";
 
+function MatchPath({ path, Comp }) {
+  let match = useMatch(path);
+  return match ? <Comp {...match} /> : null;
+}
+
 function App() {
   return (
     <Container>
       {/* <RouteController /> */}
-      <BrowserRouter>
+      <Router>
+                {/* <MatchPath path='/' Comp={Home} />
+                <MatchPath path='posts/:postId' Comp={Article} />
+                <MatchPath path='posts/' Comp={ArticleList} />
+                <MatchPath path='*' Comp={NotFound} /> */}
             <Routes>
                 <Route path='/' element={<Home />} />
                 <Route path='posts/:postId' element={<Article />} />
                 <Route path='posts/' element={<ArticleList />} />
                 <Route path='*' element={<NotFound />} />
             </Routes>
-        </BrowserRouter>
+        </Router>
     </Container>
   );
 }
