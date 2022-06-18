92dba32a3adef44b7f11b3257466de4d549d5729
Ravindra Nag <ravindranag52@gmail.com>
2022-06-18 12:00:28 +0530

    placing routes directly in App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,23 @@
 import { Container } from "@mui/material";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 import RouteController from "./controllers/RouteController";
+import Article from "./pages/Article";
+import ArticleList from "./pages/ArticleList";
+import Home from "./pages/Home";
+import NotFound from "./pages/NotFound";
 
 function App() {
   return (
     <Container>
-      <RouteController />
+      {/* <RouteController /> */}
+      <BrowserRouter>
+            <Routes>
+                <Route path='/' element={<Home />} />
+                <Route path='posts/:postId' element={<Article />} />
+                <Route path='posts/' element={<ArticleList />} />
+                <Route path='*' element={<NotFound />} />
+            </Routes>
+        </BrowserRouter>
     </Container>
   );
 }
